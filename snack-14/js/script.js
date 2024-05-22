637d4233d585595299e1c8101c1b67ec2bab6415
AdelMysterio <storliniadelmo@gmail.com>
2024-05-22 12:42:50 +0200

    svolto snack-15

--- snack-14/js/script.js
+++ snack-14/js/script.js
@@ -1,12 +1,12 @@
 // ! Chiedi all'utente un numero di secondi da attendere e fai partire un timer da quel momento a zero secondi, interrompendo il timer
 
-let userSeconds = Number.parseInt(prompt('Scrivi un numero di secondi da attendere'), 10)
-const h1Element = document.querySelector('h1')
+let userSeconds = Number.parseInt(prompt('Scrivi un numero di secondi da attendere'), 10);
+const h1Element = document.querySelector('h1');
 
 let clock = setInterval(function () {
-    userSeconds -= 1
-    console.log(userSeconds)
-    h1Element.innerHTML = userSeconds
+    userSeconds -= 1;
+    console.log(userSeconds);
+    h1Element.innerHTML = userSeconds;
 
     if (userSeconds <= 0) {
         clearInterval(clock)
